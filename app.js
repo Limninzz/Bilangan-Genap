599597f1c41d0df57b44f6f2f9b9ece03cf1897e
Limninzz <alimerlianmubarok@gmail.com>
2023-04-05 16:59:04 +0700

    4th-commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let input1="12345";
-function1(input1);
+console.log(function1(input1));
 
 function function1(input1) {
     let result =[];
@@ -7,13 +7,20 @@ function function1(input1) {
     for(let i=0; i<input1.length; i++){
         result.push(input1[i]);
     }
-    function2(result);
+    let output=function2(result);
+    return output;
 }
 
 
 function function2(input2) {
     // console.log(input2)
+    let temp=0;
     for (let i=0; i<input2.length; i++) {
-        
+        temp+=parseInt(input2[i]);
+    }
+    if (temp%2===0){
+        return "Genap";
+    } else {
+        return "Ganjil";
     }
 }
