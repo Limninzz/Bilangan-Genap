31bb69acb73d3f1c1ce5df0bedb1acfa3b4bd41b
Limninzz <alimerlianmubarok@gmail.com>
2023-04-05 16:47:41 +0700

    3rd-commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-let input1=document.getElementById("input1");
+let input1="12345";
+function1(input1);
 
-let result =[];
-for(let i=0; i<input1.length; i++){
-    result.push(input1[i]);
+function function1(input1) {
+    let result =[];
+
+    for(let i=0; i<input1.length; i++){
+        result.push(input1[i]);
+    }
+    function2(result);
 }
-console.log(result);
+
+
+function function2(input2) {
+    // console.log(input2)
+    for (let i=0; i<input2.length; i++) {
+        
+    }
+}
